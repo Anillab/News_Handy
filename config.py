ce0be61ec7d37b1c43f498bc09926f2758ab248c
Anillab <anillabusolo@gmail.com>
2018-06-15 19:23:06 +1245

    modified three files

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 class Config:
+    '''
+    This is ageneral configuration class
+    '''
     pass
 class ProdConfig(Config)
     pass
 class DevConfig(Config):
-    pass
-        
+    DEBUG= True
+config_options={
+'development':DevConfig,
+'production':ProdConfig
+}#this is a dictonary
