b90aa910470b56534164185edd46ab8bbf1db4aa
Anillab <anillabusolo@gmail.com>
2018-06-16 05:34:42 +1245

    modified one file

---
+++ app/requests.py
@@ -0,0 +1,9 @@
+import urllib.request,json
+from .models import News_Sources,News_Article
+api_key=None
+base_url=None
+def configure_request(app):
+    '''
+    a function that takes in the application instance and replaces the value of variables set to none
+    '''
+    pass
