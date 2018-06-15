30e71f20e181c433345126b32eed6f5c018f32a1
Anillab <anillabusolo@gmail.com>
2018-06-16 11:28:46 +1245

    added a function to the request.py file

--- app/requests.py
+++ app/requests.py
@@ -6,4 +6,21 @@ def configure_request(app):
     '''
     a function that takes in the application instance and replaces the value of variables set to none
     '''
-    pass
+    global api_key,base_url,news_article_url
+    api_key=app.config['news_api_key']
+    base_url=app.config['news_sources_api_base_url']
+    news_article_url=app.config['news_article_url']
+
+def get_news_sources(category):
+    '''
+    a function that gets the json response to our url request
+    '''
+    get_news_sources_url=base_url.format(category,api_key)
+    with urllib.request.urlopen(get_news_sources_url) as url:
+        get_news_sources_data=url.read()
+        get_news_sources_response=json.loads(get_news_sources_data)
+        news_sources_results=None
+        if get_news_sources_responose['results']:
+            news_sources_results_list=get_news_sources_response['results']
+            news_sources_results=process_sources_results(news_sources_results_list)
+            return news_sources_results
--- config.py
+++ config.py
@@ -3,7 +3,11 @@ class Config:
     '''
     This is ageneral configuration class
     '''
-    pass
+    news_sources_api_base_url='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    news_article_url='https://newsapi.org/v2/everthing?&apiKey={}'
+    news_api_key=os.environ.get('news_api_key')
+    secret_key=os.environ.get('secret_key')
+
 class ProdConfig(Config)
     pass
 class DevConfig(Config):
