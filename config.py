bcce2cde2a40d0da58188052f8426746636bc39c
Anillab <anillabusolo@gmail.com>
2018-06-16 23:05:34 +1245

    modified config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     This is ageneral configuration class
     '''
     news_sources_api_base_url='https://newsapi.org/v2/sources?category={}&apiKey={}'
-    news_article_url='https://newsapi.org/v2/everthing?&apiKey={}'
+    news_article_url='https://newsapi.org/v2/everything?q={}&apiKey={}'
     news_api_key=os.environ.get('news_api_key')
     secret_key=os.environ.get('secret_key')
 
