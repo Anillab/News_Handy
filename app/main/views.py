f40579816a1fa11bf6472fad66583444a74dbd0f
Anillab <anillabusolo@gmail.com>
2018-06-19 12:24:57 +1245

    created a macros.html file

--- app/main/views.py
+++ app/main/views.py
@@ -8,5 +8,6 @@ def index():
     a function that displays the main route page
     '''
     news_sources=get_news_sources('general')
+    xxx=get_news_sources('sports')
     # print(news_sources)
-    return render_template('index.html',general=news_sources)
+    return render_template('index.html',general=news_sources,sports=xxx)
